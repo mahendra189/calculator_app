9d808dd3de619714ce4384693ad64bcfb969ff83
mahendra189 <mahendrank18082004@gmail.com>
2024-10-29 17:29:58 +0530

    drawer item

---
+++ lib/widgets/drawer_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DrawerItem extends StatelessWidget {
+  final IconData? icon;
+  final Function? onClick;
+  final String? label;
+  const DrawerItem({super.key, this.icon, this.onClick, this.label});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onClick!();
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: Icon(
+                  icon != null ? icon! : CupertinoIcons.question,
+                  size: 40,
+                )),
+            Expanded(
+              child: Text(
+                label != null ? label! : 'Title',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Theme.of(context).scaffoldBackgroundColor),
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
